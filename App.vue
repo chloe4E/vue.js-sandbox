336517f4fb96d4c72dcf60d67093d154d7b82da7
Chloé Faurie <chloe.faurie@gmail.com>
2022-07-24 13:20:38 +0100

    first commit

---
+++ App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Welcome to{{ siteName }}</h1>
+</template>
+<script>
+export default {
+  name: "MyComponent",
+  data() {
+    return { siteName: "single file component in vue.js" };
+  },
+};
+</script>
+<style>
+h1 {
+  color: lightpink;
+}
+</style>
