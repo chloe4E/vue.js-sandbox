e5c5fe033b167beb4fd7c64ddd449001d41fc58b
Chloé Faurie <chloe.faurie@gmail.com>
2022-07-26 17:22:08 +0100

    chapter 5 done

--- ecommerce-example/src/pages/EmployeeOfTheMonth.vue
+++ ecommerce-example/src/pages/EmployeeOfTheMonth.vue
@@ -1,21 +1,23 @@
 <template>
-  <h1>Employee Of The Month</h1>
+  <h1 v-flash="1000">Employee Of The Month</h1>
   <div v-if="personData">
     <h3>{{ personData.name.first }} {{ personData.name.last }}</h3>
   </div>
 </template>
 
 <script>
-import axios from "axios";
+//import axios from "axios";
+import randomPersonMixin from "../mixins/randomPersonMixin";
 export default {
   name: "EmployeeOfTheMonthPage",
-  data() {
+  mixins: [randomPersonMixin],
+  /*  data() {
     return { personData: null };
   },
   created() {
     axios
       .get("https://randomuser.me/api/")
       .then((response) => (this.personData = response.data.results[0]));
-  },
+  }, */
 };
 </script>
